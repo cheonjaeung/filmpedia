71f274876cda95187e1e3d86657dba26b82af4b3
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-08-03 15:52:40 +0900

    feat: connect api logic and home activity ui

---
+++ app/src/main/java/io/woong/filmpedia/adapter/Top10MovieListAdapter.kt
@@ -0,0 +1,48 @@
+package io.woong.filmpedia.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import io.woong.filmpedia.R
+import io.woong.filmpedia.data.Movies
+import io.woong.filmpedia.util.ImagePathUtil
+
+class Top10MovieListAdapter(private val context: Context) : RecyclerView.Adapter<Top10MovieListAdapter.ViewHolder>() {
+
+    private val top10: MutableList<Movies.Result> = mutableListOf()
+
+    fun setTop10(top10: List<Movies.Result>) {
+        this.top10.clear()
+        this.top10.addAll(top10)
+        notifyDataSetChanged()
+    }
+
+    fun getTop10(): List<Movies.Result> = top10
+
+    override fun getItemCount(): Int = top10.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val view = inflater.inflate(R.layout.layout_top10_movie_list_item_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val posterPath = top10[position].posterPath
+        posterPath?.let { path ->
+            Glide.with(context)
+                .load(ImagePathUtil.toFullUrl(path))
+                .into(holder.posterView)
+        }
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val posterView: AppCompatImageView = itemView.findViewById(R.id.t10mliv_poster)
+    }
+}
--- app/src/main/java/io/woong/filmpedia/ui/activity/home/HomeActivity.kt
+++ app/src/main/java/io/woong/filmpedia/ui/activity/home/HomeActivity.kt
@@ -3,6 +3,8 @@ package io.woong.filmpedia.ui.activity.home
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.woong.filmpedia.adapter.Top10MovieListAdapter
 import io.woong.filmpedia.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -15,9 +17,46 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.recommendedMovie.observe(this) {
-            binding.recommendedMovie.movie = it
+        viewModel.recommendedMovie.observe(this) { movie ->
+            binding.recommendedMovie.movie = movie
         }
+
+        binding.top10NowPlayingList.apply {
+            adapter = Top10MovieListAdapter(this@HomeActivity)
+            layoutManager = LinearLayoutManager(this@HomeActivity, LinearLayoutManager.HORIZONTAL, false)
+        }
+        viewModel.top10NowPlayingMovies.observe(this) { movies ->
+            val adapter = binding.top10NowPlayingList.adapter as Top10MovieListAdapter
+            adapter.setTop10(movies)
+        }
+
+        binding.top10PopularList.apply {
+            adapter = Top10MovieListAdapter(this@HomeActivity)
+            layoutManager = LinearLayoutManager(this@HomeActivity, LinearLayoutManager.HORIZONTAL, false)
+        }
+        viewModel.top10PopularMovies.observe(this) { movies ->
+            val adapter = binding.top10PopularList.adapter as Top10MovieListAdapter
+            adapter.setTop10(movies)
+        }
+
+        binding.top10RatedList.apply {
+            adapter = Top10MovieListAdapter(this@HomeActivity)
+            layoutManager = LinearLayoutManager(this@HomeActivity, LinearLayoutManager.HORIZONTAL, false)
+        }
+        viewModel.top10RatedMovies.observe(this) { movies ->
+            val adapter = binding.top10RatedList.adapter as Top10MovieListAdapter
+            adapter.setTop10(movies)
+        }
+
+        binding.top10UpcomingList.apply {
+            adapter = Top10MovieListAdapter(this@HomeActivity)
+            layoutManager = LinearLayoutManager(this@HomeActivity, LinearLayoutManager.HORIZONTAL, false)
+        }
+        viewModel.top10UpcomingMovies.observe(this) { movies ->
+            val adapter = binding.top10UpcomingList.adapter as Top10MovieListAdapter
+            adapter.setTop10(movies)
+        }
+
         viewModel.update()
     }
 }
