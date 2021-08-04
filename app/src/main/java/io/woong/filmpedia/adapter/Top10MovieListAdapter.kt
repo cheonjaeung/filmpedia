a3705bf5e1f24853e392d1b5ad8407f67bfc47f4
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-08-04 12:21:56 +0900

    feat: set corner roundness in home movie list item view

--- app/src/main/java/io/woong/filmpedia/adapter/Top10MovieListAdapter.kt
+++ app/src/main/java/io/woong/filmpedia/adapter/Top10MovieListAdapter.kt
@@ -1,12 +1,15 @@
 package io.woong.filmpedia.adapter
 
 import android.content.Context
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import io.woong.filmpedia.R
 import io.woong.filmpedia.data.Movies
 import io.woong.filmpedia.util.ImagePathUtil
@@ -35,8 +38,15 @@ class Top10MovieListAdapter(private val context: Context) : RecyclerView.Adapter
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val posterPath = top10[position].posterPath
         posterPath?.let { path ->
+            val radiusDp = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                4f,
+                context.resources.displayMetrics
+            ).toInt()
+
             Glide.with(context)
                 .load(ImagePathUtil.toFullUrl(path))
+                .apply(RequestOptions.bitmapTransform(RoundedCorners(radiusDp)))
                 .into(holder.posterView)
         }
     }
