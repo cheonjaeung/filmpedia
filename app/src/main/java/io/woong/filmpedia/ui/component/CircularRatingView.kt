1bfbb6f3d70789313dadd9c023f14ef6b851b531
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-08-09 15:25:23 +0900

    feat: create CircularRatingView for displaying vote average

---
+++ app/src/main/java/io/woong/filmpedia/ui/component/CircularRatingView.kt
@@ -0,0 +1,209 @@
+package io.woong.filmpedia.ui.component
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.View
+import androidx.annotation.ColorInt
+import io.woong.filmpedia.R
+
+class CircularRatingView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : View(context, attrs, defStyle) {
+
+    companion object {
+        private const val RATING_ARC_START_ANGLE: Float = -90f
+    }
+
+    var rating: Int = 0
+        set(value) {
+            field = value
+            invalidate()
+        }
+    private var ratingMax: Int = 0
+    @ColorInt
+    private var ratingTextColor: Int = 0
+    private var ratingTextSize: Float = 0f
+    private val ratingTextRect: Rect = Rect()
+    private val ratingTextPaint: Paint = Paint().apply { isAntiAlias = true }
+
+    @ColorInt
+    private var backgroundCircleColor: Int = 0
+    private var circleCenterX: Float = 0f
+    private var circleCenterY: Float = 0f
+    private var backgroundCircleRadius: Float = 0f
+    private val backgroundCirclePaint: Paint = Paint().apply { isAntiAlias = true }
+
+    @ColorInt
+    private var ratingArcColor: Int = 0
+    private var ratingArcSize: Float = 0f
+    private val ratingArcRect: RectF = RectF()
+    private var ratingArcAngle: Float = 0f
+    private val ratingArcPaint: Paint = Paint().apply { isAntiAlias = true }
+
+    @ColorInt
+    private var centerCircleColor: Int = 0
+    private var centerCircleRadius: Float = 0f
+    private val centerCirclePaint: Paint = Paint().apply { isAntiAlias = true }
+
+    init {
+        applyAttributes(attrs, defStyle)
+    }
+
+    private fun applyAttributes(attributes: AttributeSet?, defStyle: Int) {
+        val attrs = context.obtainStyledAttributes(
+            attributes,
+            R.styleable.CircularRatingView,
+            defStyle,
+            0
+        )
+
+        try {
+            rating = attrs.getInt(
+                R.styleable.CircularRatingView_rating,
+                0
+            )
+
+            ratingMax = attrs.getInt(
+                R.styleable.CircularRatingView_rating_max,
+                100
+            )
+
+            ratingArcSize = attrs.getDimension(
+                R.styleable.CircularRatingView_rating_arc_size,
+                TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    4f,
+                    context.resources.displayMetrics
+                )
+            )
+
+            ratingArcColor = attrs.getColor(
+                R.styleable.CircularRatingView_rating_arc_color,
+                Color.GREEN
+            )
+
+            ratingTextSize = attrs.getDimension(
+                R.styleable.CircularRatingView_rating_text_size,
+                TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_SP,
+                    10f,
+                    context.resources.displayMetrics
+                )
+            )
+
+            ratingTextColor = attrs.getColor(
+                R.styleable.CircularRatingView_rating_text_color,
+                Color.WHITE
+            )
+
+            backgroundCircleColor = attrs.getColor(
+                R.styleable.CircularRatingView_background_circle_color,
+                Color.LTGRAY
+            )
+
+            centerCircleColor = attrs.getColor(
+                R.styleable.CircularRatingView_center_circle_color,
+                Color.DKGRAY
+            )
+        } finally {
+            attrs.recycle()
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+        setMeasuredDimension(width, height)
+
+        circleCenterX = (paddingLeft + (width - paddingRight)) / 2f
+        circleCenterY = (paddingTop + (height - paddingBottom)) / 2f
+
+        backgroundCircleRadius = if (width < height) {
+            (width - paddingLeft - paddingRight) / 2f
+        } else {
+            (height - paddingTop - paddingBottom) / 2f
+        }
+
+        ratingArcRect.set(
+            circleCenterX - backgroundCircleRadius,
+            circleCenterY - backgroundCircleRadius,
+            circleCenterX + backgroundCircleRadius,
+            circleCenterY + backgroundCircleRadius
+        )
+
+        centerCircleRadius = backgroundCircleRadius - ratingArcSize
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        val percent = rating.toFloat() / ratingMax.toFloat()
+
+        backgroundCirclePaint.color = backgroundCircleColor
+
+        ratingArcPaint.color = ratingArcColor
+        ratingArcAngle = percent * 360.0f
+
+        centerCirclePaint.color = centerCircleColor
+
+        ratingTextPaint.apply {
+            color = ratingTextColor
+            textSize = ratingTextSize
+        }
+
+        measureTextRect("$rating%")
+
+        canvas.apply {
+            drawCircle(
+                circleCenterX,
+                circleCenterY,
+                backgroundCircleRadius,
+                backgroundCirclePaint
+            )
+
+            drawArc(
+                ratingArcRect,
+                RATING_ARC_START_ANGLE,
+                ratingArcAngle,
+                true,
+                ratingArcPaint
+            )
+
+            drawCircle(
+                circleCenterX,
+                circleCenterY,
+                centerCircleRadius,
+                centerCirclePaint
+            )
+
+            drawText(
+                "$rating%",
+                ratingTextRect.left.toFloat(),
+                ratingTextRect.bottom.toFloat(),
+                ratingTextPaint
+            )
+        }
+    }
+
+    private fun measureTextRect(text: String) {
+        ratingTextPaint.getTextBounds(
+            text,
+            0,
+            text.length,
+            ratingTextRect
+        )
+
+        val width = ratingTextRect.width()
+        val height = ratingTextRect.height()
+
+        val left = (circleCenterX - (width / 2)).toInt()
+        val top = (circleCenterY - (height / 2)).toInt()
+        val right = (circleCenterX + (width / 2)).toInt()
+        val bottom = (circleCenterY + (height / 2)).toInt()
+
+        ratingTextRect.set(left, top, right, bottom)
+    }
+}
