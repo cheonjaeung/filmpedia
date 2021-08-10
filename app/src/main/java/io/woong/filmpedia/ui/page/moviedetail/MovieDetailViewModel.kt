d5b079ef19b596900ed02952dd2936f111bfe097
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-08-10 17:55:03 +0900

    feat: connect credits fetching logic with MovieDetailViewModel

--- app/src/main/java/io/woong/filmpedia/ui/page/moviedetail/MovieDetailViewModel.kt
+++ app/src/main/java/io/woong/filmpedia/ui/page/moviedetail/MovieDetailViewModel.kt
@@ -3,10 +3,12 @@ package io.woong.filmpedia.ui.page.moviedetail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.woong.filmpedia.data.Credits
 import io.woong.filmpedia.data.Movie
 import io.woong.filmpedia.repository.MovieRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 
 class MovieDetailViewModel : ViewModel() {
@@ -21,15 +23,27 @@ class MovieDetailViewModel : ViewModel() {
     val movie: LiveData<Movie>
         get() = _movie
 
+    private val _credits: MutableLiveData<Credits> = MutableLiveData()
+    val credits: LiveData<Credits>
+        get() = _credits
+
     fun update(movieId: Int) {
         CoroutineScope(Dispatchers.Default).launch {
             _isLoading.postValue(true)
 
-            repository.fetchMovieDetail(movieId = movieId) { movie ->
+            val detailFetchingJob = repository.fetchMovieDetail(movieId = movieId) { movie ->
                 movie?.let { m ->
                     _movie.postValue(m)
                 }
-            }.join()
+            }
+
+            val creditsFetchingJob = repository.fetchCredits(movieId = movieId) { credits ->
+                credits?.let { c ->
+                    _credits.postValue(c)
+                }
+            }
+
+            joinAll(detailFetchingJob, creditsFetchingJob)
 
             _isLoading.postValue(false)
         }
