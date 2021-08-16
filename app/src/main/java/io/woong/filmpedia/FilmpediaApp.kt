ddbe6d4a8501049834cc3bf8c197b800fb700779
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-08-16 16:13:06 +0900

    feat: create app class for getting language and region

--- app/src/main/java/io/woong/filmpedia/FilmpediaApp.kt
+++ app/src/main/java/io/woong/filmpedia/FilmpediaApp.kt
@@ -1,5 +1,52 @@
 package io.woong.filmpedia
 
-/** TMDb API key. */
-val apiKey: String
-    get() = BuildConfig.API_KEY
+import android.app.Application
+import android.os.Build
+
+class FilmpediaApp : Application() {
+
+    val tmdbApiKey: String
+        get() = BuildConfig.API_KEY
+
+    val language: String
+        get() {
+            val locale = if (Build.VERSION.SDK_INT >= 24) {
+                resources.configuration.locales[0]
+            } else {
+                resources.configuration.locale
+            }
+
+            return when {
+                locale == null -> {
+                    "en-US"
+                }
+                locale.toString().startsWith("en") -> {
+                    "en-US"
+                }
+                locale.toString().startsWith("ko") -> {
+                    "ko-KR"
+                }
+                else -> {
+                    "en-US"
+                }
+            }
+        }
+
+    val region: String
+        get() {
+            val locale = if (Build.VERSION.SDK_INT >= 24) {
+                resources.configuration.locales[0]
+            } else {
+                resources.configuration.locale
+            }
+
+            return when {
+                locale == null -> {
+                    "US"
+                }
+                else -> {
+                    locale.country
+                }
+            }
+        }
+}
