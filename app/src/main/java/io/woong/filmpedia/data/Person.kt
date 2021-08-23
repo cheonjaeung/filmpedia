721b4601daed9c1fb8809afdbe3492eb4f93d310
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-08-23 17:34:23 +0900

    refactor: reorder Person's properties by alphabetically

--- app/src/main/java/io/woong/filmpedia/data/Person.kt
+++ app/src/main/java/io/woong/filmpedia/data/Person.kt
@@ -3,18 +3,18 @@ package io.woong.filmpedia.data
 import com.google.gson.annotations.SerializedName
 
 data class Person(
-    val birthday: String?,
-    @SerializedName("known_for_department") val knownForDepartment: String,
-    val deathDay: String?,
-    val id: Int,
-    val name: String,
-    @SerializedName("also_known_as") val alsoKnownAs: List<String>,
-    val gender: Int,
-    val biography: String,
-    val popularity: Double,
-    @SerializedName("place_of_birth") val placeOfBirth: String?,
-    @SerializedName("profile_path") val profilePath: String?,
     val adult: Boolean,
+    @SerializedName("also_known_as") val alsoKnownAs: List<String>,
+    val biography: String,
+    val birthday: String?,
+    val deathDay: String?,
+    val gender: Int,
+    val homepage: String?,
+    val id: Int,
     @SerializedName("imdb_id") val imdbId: String,
-    val homepage: String?
+    @SerializedName("known_for_department") val knownForDepartment: String,
+    val name: String,
+    @SerializedName("place_of_birth") val placeOfBirth: String?,
+    val popularity: Double,
+    @SerializedName("profile_path") val profilePath: String?,
 )
