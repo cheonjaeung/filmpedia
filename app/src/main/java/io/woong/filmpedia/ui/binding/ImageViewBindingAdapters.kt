7d7d69131ed75bff85258b76fc9ea2b30320ef2f
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-09-03 20:49:44 +0900

    refactor: integrate 3 image binding adapters to 1

--- app/src/main/java/io/woong/filmpedia/ui/binding/ImageViewBindingAdapters.kt
+++ app/src/main/java/io/woong/filmpedia/ui/binding/ImageViewBindingAdapters.kt
@@ -1,37 +1,25 @@
 package io.woong.filmpedia.ui.binding
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import io.woong.filmpedia.R
 import io.woong.filmpedia.util.UriUtil
 
 object ImageViewBindingAdapters {
 
     @JvmStatic
-    @BindingAdapter("poster")
-    fun AppCompatImageView.bindPosterImage(posterPath: String?) {
-        Glide.with(this)
-            .load(UriUtil.getImageUrl(posterPath))
-            .placeholder(R.drawable.placeholder_poster)
-            .into(this)
-    }
-
-    @JvmStatic
-    @BindingAdapter("profile")
-    fun AppCompatImageView.bindProfileImage(posterPath: String?) {
-        Glide.with(this)
-            .load(UriUtil.getImageUrl(posterPath))
-            .placeholder(R.drawable.placeholder_profile)
-            .into(this)
-    }
-
-    @JvmStatic
-    @BindingAdapter("backdrop")
-    fun AppCompatImageView.bindBackdropImage(posterPath: String?) {
-        Glide.with(this)
-            .load(UriUtil.getImageUrl(posterPath))
-            .placeholder(R.drawable.placeholder_backdrop)
-            .into(this)
+    @BindingAdapter(value = ["glide_image", "glide_placeholder"], requireAll = false)
+    fun AppCompatImageView.bindGlideImage(imagePath: String?, placeholder: Drawable?) {
+        if (placeholder != null) {
+            Glide.with(this)
+                .load(UriUtil.getImageUrl(imagePath))
+                .placeholder(placeholder)
+                .into(this)
+        } else {
+            Glide.with(this)
+                .load(UriUtil.getImageUrl(imagePath))
+                .into(this)
+        }
     }
 }
