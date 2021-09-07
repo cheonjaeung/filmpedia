476d5ccbdc5a0771d7dd3a4c22ab1e1cb372bd06
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-09-07 22:17:13 +0900

    feat: create blink animation

--- app/src/main/java/io/woong/filmpedia/util/AnimationUtil.kt
+++ app/src/main/java/io/woong/filmpedia/util/AnimationUtil.kt
@@ -1,5 +1,7 @@
 package io.woong.filmpedia.util
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.Interpolator
@@ -33,5 +35,20 @@ object AnimationUtil {
             this.duration = duration
         }
         view.startAnimation(animation)
+    }
+
+    fun blink(
+        view: View,
+        duration: Long,
+        interpolator: Interpolator = LinearInterpolator(),
+        startDelay: Long = 0
+    ) {
+        ObjectAnimator.ofFloat(view, "alpha", 0.0f).apply {
+            this.interpolator = interpolator
+            this.duration = duration / 2
+            this.startDelay = startDelay
+            this.repeatCount = ValueAnimator.INFINITE
+            this.repeatMode = ValueAnimator.REVERSE
+        }.start()
     }
 }
