8829e6b6cc4b685d82adcb5189bf0f351959aee7
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-09-07 16:46:01 +0900

    feat: create gender and birthday/deathday/age binding adapter for textview

--- app/src/main/java/io/woong/filmpedia/ui/binding/TextViewBindingAdapters.kt
+++ app/src/main/java/io/woong/filmpedia/ui/binding/TextViewBindingAdapters.kt
@@ -4,6 +4,8 @@ import androidx.annotation.PluralsRes
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.BindingAdapter
 import io.woong.filmpedia.R
+import io.woong.filmpedia.data.people.Person
+import io.woong.filmpedia.util.DateUtil
 import java.lang.StringBuilder
 import java.text.DecimalFormat
 
@@ -24,6 +26,17 @@ object TextViewBindingAdapters {
             }
 
             this.text = builder.toString()
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["plurals_resource", "plurals_count", "plurals_list"], requireAll = false)
+    fun AppCompatTextView.bindPlurals(@PluralsRes res: Int?, count: Int?, list: List<Any>?) {
+        val c = list?.size ?: 2
+
+        if (res != null) {
+            val plurals = resources.getQuantityText(res, c)
+            this.text = plurals
         }
     }
 
@@ -54,13 +67,38 @@ object TextViewBindingAdapters {
     }
 
     @JvmStatic
-    @BindingAdapter(value = ["plurals_resource", "plurals_count", "plurals_list"], requireAll = false)
-    fun AppCompatTextView.bindPlurals(@PluralsRes res: Int?, count: Int?, list: List<Any>?) {
-        val c = list?.size ?: 2
+    @BindingAdapter("gender")
+    fun AppCompatTextView.bindGender(gender: Person.Gender?) {
+        if (gender != null) {
+            val genderString: String = when (gender) {
+                Person.Gender.UNSPECIFIED -> resources.getString(R.string.person_gender_unspecified)
+                Person.Gender.MALE -> resources.getString(R.string.person_gender_male)
+                Person.Gender.FEMALE -> resources.getString(R.string.person_gender_female)
+                Person.Gender.NON_BINARY -> resources.getString(R.string.person_gender_non_binary)
+            }
 
-        if (res != null) {
-            val plurals = resources.getQuantityText(res, c)
-            this.text = plurals
+            this.text = genderString
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["birthday", "deathday"], requireAll = false)
+    fun AppCompatTextView.bindLife(birthday: String?, deathday: String?) {
+        if (birthday != null) {
+            val age = DateUtil.getAge(birthday, deathday, -1)
+
+            val builder = StringBuilder(birthday)
+
+            if (deathday != null) {
+                builder.append("~")
+                builder.append(" ")
+                builder.append("$deathday")
+            }
+
+            builder.append(" ")
+            builder.append("($age)")
+
+            this.text = builder.toString()
         }
     }
 }
