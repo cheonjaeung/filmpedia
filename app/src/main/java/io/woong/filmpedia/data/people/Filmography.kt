c8ca233f7f4293e7b7bab159444766d14903e6fd
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-09-08 18:13:16 +0900

    refactor: make Filmography data class to sealed class

--- app/src/main/java/io/woong/filmpedia/data/people/Filmography.kt
+++ app/src/main/java/io/woong/filmpedia/data/people/Filmography.kt
@@ -1,9 +1,32 @@
 package io.woong.filmpedia.data.people
 
-data class Filmography(
-    val id: Int,
-    val viewType: Int,
-    val releasedYear: String,
-    val movieTitle: String,
-    val department: String
-)
+import io.woong.filmpedia.ui.page.person.FilmographyAdapter
+
+sealed class Filmography(val viewType: FilmographyAdapter.ViewType) {
+
+    data class Profile(
+        val name: String,
+        val profilePath: String?,
+        val birthday: String?,
+        val deathday: String?,
+        val gender: Person.Gender,
+        val placeOfBirth: String?
+    ) : Filmography(FilmographyAdapter.ViewType.PROFILE)
+
+    data class Biography(
+        val content: String?
+    ) : Filmography(FilmographyAdapter.ViewType.BIOGRAPHY)
+
+    data class Title(
+        val text: String
+    ) : Filmography(FilmographyAdapter.ViewType.TITLE)
+
+    data class Movie(
+        val id: Int,
+        val title: String,
+        val releaseYear: String,
+        val department: String
+    ) : Filmography(FilmographyAdapter.ViewType.MOVIE)
+
+    class Divider : Filmography(FilmographyAdapter.ViewType.DIVIDER)
+}
